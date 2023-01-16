854b37f133391912a5ca3985e6e7667b54331b6d
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-01-16 17:26:13 -0300

    Aula 110 - Autoexectar uma funcao com IIFE

---
+++ funcao/iife.js
@@ -0,0 +1,10 @@
+//IIFE => Immediately Invoked Function Expression
+
+/**Dessa forma a função é autoexecutada 
+ * devido a estar dentro de um parenteses 
+ * e no final ter o ()
+*/
+(function () {
+  console.log("Será executada na hora!");
+  console.log("Foge do escopo mais abrangente!");
+})()
