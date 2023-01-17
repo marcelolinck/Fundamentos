39f7ca3d99e33042301beb8d4f237ca820e75694
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-01-16 23:44:08 -0300

    Aula 114 - Iniciando os principios da OO

---
+++ objeto/oo.js
@@ -0,0 +1,32 @@
+//CODIGO NÃO EXECUTÁVEL
+
+//PROCEDURAL
+
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //....funcoes ou comportamentos
+    }
+}
+/**
+ * Dai consigo chamar a função especifica
+ * a partir deste objeto (atributos e comportamentos)
+*/
+objeto.processamento()
+
+//Principios importantes:
+/*
+1. Abstracao
+2. Encapsulamento
+3. Polimorfismo
+Multiplas formas onde utilizar uma variavel de modo genérico
+e reaproveitar para outro contexto.
+4. Herança(prototype)
+    Recebe caracteristicas e atributos do objeto pai
+*/
